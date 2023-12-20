c1191dbaff3d8176834e428d8ad2d6e9893a58b6
TanishaK20 <tanishatanvi0320@gmail.com>
2023-12-20 19:00:00 +0530

    initial

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnSyntaxCheck = new System.Windows.Forms.Button();
+            this.txtProgram = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(30, 317);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(347, 20);
+            this.txtCommand.TabIndex = 0;
+            this.txtCommand.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.rtbOutput.Location = new System.Drawing.Point(422, 12);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(350, 282);
+            this.rtbOutput.TabIndex = 2;
+            this.rtbOutput.Text = "";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(30, 343);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(85, 29);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSyntaxCheck
+            // 
+            this.btnSyntaxCheck.Location = new System.Drawing.Point(140, 343);
+            this.btnSyntaxCheck.Name = "btnSyntaxCheck";
+            this.btnSyntaxCheck.Size = new System.Drawing.Size(82, 29);
+            this.btnSyntaxCheck.TabIndex = 4;
+            this.btnSyntaxCheck.Text = "Syntax";
+            this.btnSyntaxCheck.UseVisualStyleBackColor = true;
+            // 
+            // txtProgram
+            // 
+            this.txtProgram.Location = new System.Drawing.Point(30, 12);
+            this.txtProgram.Multiline = true;
+            this.txtProgram.Name = "txtProgram";
+            this.txtProgram.Size = new System.Drawing.Size(333, 282);
+            this.txtProgram.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtProgram);
+            this.Controls.Add(this.btnSyntaxCheck);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.txtCommand);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnSyntaxCheck;
+        private System.Windows.Forms.TextBox txtProgram;
+    }
+}
+
